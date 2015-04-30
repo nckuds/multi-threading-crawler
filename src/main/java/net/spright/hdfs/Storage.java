ff09285a9c6b08e9948821bfc339eb5109a59782
chihsuan <chihsuan.huang.7@gmail.com>
2015-04-30 20:15:02 +0800

    remove files & update carwler

--- src/main/java/net/spright/hdfs/Storage.java
+++
@@ -1,9 +0,0 @@
-package net.spright.hdfs;
-
-import java.io.Closeable;
-import java.io.IOException;
-
-public interface Storage extends Closeable {
-    public abstract UrlSource.Writer createWriter() throws IOException;
-    public abstract UrlSource.Scanner createScanner() throws IOException;
-}
